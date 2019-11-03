4488171b387ecea6c685a976dcc0a51eece5ab4c
JOSE\jr607 <joseromero611@gmail.com>
2019-11-03 01:39:52 -0400

    Minecraft to text request
    Minecraft /me command can be used to give code msg and destination

--- src/MCParser.java
+++ src/MCParser.java
@@ -4,17 +4,18 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.text.ParseException;
 
 public class MCParser {
 	
 	//main function to read log of server, parse messages, and send them to the desired phone number
-	public static void main(String[] args) throws FileNotFoundException, URISyntaxException {
+	public static void main(String[] args) throws FileNotFoundException, URISyntaxException, ParseException {
 		
 		MCLogReader reader = new MCLogReader();
-		reader.parseLog("[22:34:56]");
-//		File path = new File(new URI("file:///Users/jr607/Desktop/1.14.4%20Server/logs/latest.log"));
-//		reader.stream = new BufferedInputStream(new FileInputStream(path));
-//		reader.start();
+//		reader.parseLog("[12:14:56]");
+		File path = new File(new URI("file:///Users/jr607/Desktop/1.14.4%20Server/logs/latest.log"));
+		reader.stream = new BufferedInputStream(new FileInputStream(path));
+		reader.start();
 		
 	}
 }
--- src/main.java
+++ src/main.java
@@ -1,9 +1,9 @@
 
 public class main {
-
+	
 	public static void main(String[] args) {
-		SmsReceiver test = new SmsReceiver();
-		String testString = test.getMessages();
-		System.out.println(testString);
+		//SmsReceiver test = new SmsReceiver();
+		//String testString = test.getMessages();
+		//System.out.println(testString);
 	}
 }
