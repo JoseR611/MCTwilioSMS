9066393ee4830d1f665ee90ccc75d936c2e686d1
JOSE\jr607 <joseromero611@gmail.com>
2019-11-03 05:33:51 -0500

    asd

---
+++ src/Startup.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Startup {
+    public static void main(String[] args) throws Exception {
+        ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/c",  args[0]);
+        builder.redirectErrorStream(true);
+        Process p = builder.start();
+        BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        String line;
+        while (true) {
+            line = r.readLine();
+            if (line == null) { break; }
+            System.out.println(line);
+        }
+    }
+}
