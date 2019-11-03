f269efd138b35019bd70ce58614cf5626a1a58f8
JOSE\jr607 <joseromero611@gmail.com>
2019-11-03 09:55:01 -0500

    almost final

--- src/MCParser.java
+++ src/MCParser.java
@@ -7,29 +7,31 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.text.ParseException;
+import java.util.concurrent.TimeUnit;
 
 public class MCParser {
 	
 	//main function to read log of server, parse messages, and send them to the desired phone number
-	public static void main(String[] args) throws URISyntaxException, ParseException, IOException {	
-		Runtime rt = Runtime.getRuntime();
-		String[] start = new String[7];
-		start[0] = "cmd.exe";
-		start[1] = "/k";
-		start[2] = "start";
-		start[3] = "cmd.exe";
-		start[4] = "/K";
-		start[5] = "\"cd";
-		start[6] = "src";
-		Process p = rt.exec("cmd /c start cmd.exe /K \"cd \"src");
-		String test = "dir";
+	public static void main(String[] args) throws URISyntaxException, ParseException, IOException, InterruptedException {	
+		String[] command = {"cmd",};
+		Process p;
+		try {
+			p = Runtime.getRuntime().exec(command);
+		    new Thread(new Sync(p.getErrorStream(), System.err)).start();
+	        new Thread(new Sync(p.getInputStream(), System.out)).start();
+	        PrintWriter stdin = new PrintWriter(p.getOutputStream());
+	        stdin.println("hostname");
+	        stdin.close();
+	        p.waitFor();
+	    } catch (Exception e) {
+	 		e.printStackTrace();
+		}
 		
-		p.getOutputStream().write(test.getBytes());
-		//rt.exec("cmd.exe \"dir");
-		//rt.exec("cd src");
+		
 //		MCLogReader reader = new MCLogReader();
 //		reader.stream = new BufferedInputStream(new FileInputStream("src/1.14.4Server/logs/latest.log"));
 //		reader.start();
